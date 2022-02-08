d03396040c807d297a03b7b3f3336d3474025577
RongfengLi <tx771221560@163.com>
2022-02-08 15:15:15 +0800

    第一次向GITHub提交代码

---
+++ demo01.py
@@ -0,0 +1,40 @@
+# print("hello world!") #字符串
+# print(23321)   #整数
+# print(1.21021)  #小数
+# print(True)   #布尔值   true false
+# print(())  #元组
+# print([])  #数组
+# print({})  #字典
+# print("哈 哈",1515,1.24)
+# print("哈哈"+"嘻嘻") # 字符串的拼接  只能同类型的进行拼接  
+# print("哈哈"*5)    # 输出5遍
+# print(1+2*5-10+(55.5))
+# print(2>3)
+# a = "张绍"   #变量必须是小写字母
+# print(a)
+# a = input("请输入a的值")  #只要是通过input获取的都是字符串
+# b = input("请输入b的值")  #只要是通过input获取的都是字符串
+# print("a+b=",a+b)
+# 数据格式的转换
+# print(type(2333))
+# print(type(2.33))
+# print(type("哈哈"))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# a = str(2333)
+# print(type(a))  #数据转换的规律 任何数据都可以转换为字符串
+# #数据转换的规律 任何数据都可以转换为字符串除了None
+# #整数和小数可以互相转换
+# #字符串转换为其它类型的数据 需要满足一个规律 "长得像"  
+
+# a = float(input("请输入a的值"))  #只要是通过input获取的都是字符串
+# b = float(input("请输入b的值"))  #只要是通过input获取的都是字符串
+# print("a+b=",a+b)
+
+#len() 计算 字符串 元组 数组 字典的长度
+a = input("请输入a的值")
+b = input("请输入b的值")
+print("字符串a+b=",len(a)+len(b))
+
