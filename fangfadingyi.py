34e6eb00a64e59436484b8bd32475c5dddaaea96
RongfengLi <tx771221560@163.com>
2022-02-28 16:47:53 +0800

    学方法定义中

--- fangfadingyi.py
+++ fangfadingyi.py
@@ -19,18 +19,58 @@
 
 #写个2个数据相加的方法
 
-def qiuhe(a,b,c):
-    '''
-    这个方法的作用是实现3个数字相加
-    '''
-    if type(a) is int and type(b) is int and type(c) is int: #判断类型
-        return a+b+c
-    else:
-        return "输入的数据类型不正确"
+# def qiuhe(a,b,c):   #括号里面放参数
+#     '''
+#     这个方法的作用是实现3个数字相加
+#     '''
+#     if type(a) is int and type(b) is int and type(c) is int: #判断类型
+#         return a+b+c          #return返回的值可以做其它操作  print不行
+#     else:
+#         return "输入的数据类型不正确"  
 
-# c = []
-# print(c.append("哈哈"))  #输出结果返回值为None  是因为append 方法无返回值
+# # c = []
+# # print(c.append("哈哈"))  #输出结果返回值为None  是因为append 方法无返回值
 
-print(qiuhe(3,5,7))   #返回值返回后我们可以对这个值做其它操作
+# print(qiuhe(3,5,7))   #返回值返回后我们可以对这个值做其它操作
+
+# 把之前登入判断的作业写成方法
+
+# def signin(username,password):
+#     file = {}
+#     if len(username) >= 5 and len(username)<= 8:
+#         if username [0] in "qwertyuiopasdfghjklzxcvbnm":   #判断首字符是否再26个小写字母里面
+#             if len(password) >=8 and len(password) <= 12:
+#                 file["username"] = username
+#                 file["password"] = password
+#                 print("注册成功")
+#                 return True
+#             else:
+#                 print("密码长度不对")
+#         else:
+#             print("账号必须小写字母开头")        
+#     else:
+#         print("用户名长度不对")
+
+# username = input("请输入你的姓名:")
+# password = input("请输入你的密码:")
+# signin(username,password)
+
+
+
+
+
+#try + except 异常捕获   可用于自动化脚本断言
+try:
+    print("a"+y)
+except:
+    print("上面的代码写错了")
+
+try:
+    print(a*b)
+except Exception as e:   #Exception 就是把报错信息赋值给变量e并打印出来
+    print("上面的代码写错了",e)
+
+#报错信息就是从异常类来的
+# 包》类》方法》变量
 
 
