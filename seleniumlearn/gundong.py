e4ead0e6d3f0615035a4fc280ad01cfb7d525541
RongfengLi <tx771221560@163.com>
2022-03-16 11:12:11 +0800

    学习了滚动

--- seleniumlearn/gundong.py
+++ seleniumlearn/gundong.py
@@ -8,16 +8,17 @@ import time
 
 #如果没放到环境变量中则要参数指定   设置环境变量的目的就是任意时候都可以直接使用文件
 driver = webdriver.Chrome(executable_path="./chromedriver.exe")
-url = "https://www.huya.com/l"
+url = "https://www.douyu.com/directory/all"  #斗鱼直播页签页
 
 
 driver.get(url)
 driver.maximize_window()       
 time.sleep(3)
 
-
+#滚动调整到对应位置
 driver.execute_script("window.scrollTo(0,10000)")
 time.sleep(3)
 
+driver.find_element_by_xpath("//*[text()='下一页']").click()
 
-driver.find_element_by_xpath('//a[@class="laypage_next"]').click()
+driver.quit()
